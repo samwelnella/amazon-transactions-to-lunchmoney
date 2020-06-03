9633419e0ce70688dd581d56e3e4ee2d2e3a3194
samwelnella <danielsamwel@gmail.com>
2020-06-02 21:16:27 -0400

    Update update_lunchmoney_from_amazon.py

--- update_lunchmoney_from_amazon.py
+++ update_lunchmoney_from_amazon.py
@@ -1,10 +1,17 @@
 import requests
 import csv
 import pandas
+import keyring
 from datetime import datetime
 from datetime import timedelta
 
-headers = {'Authorization': 'Bearer YOUR_LUNCH_MONEY_API_KEY_HERE'}
+service_id = 'AMAZON_TO_LM'
+
+#Set your lmauth variable with keyring.set_password(service_id, 'lmauth', 'YOUR_LUNCH_MONEY_API_KEY_HERE')
+lmauth = keyring.get_password(service_id, 'lmauth')
+headers = {'Authorization': 'Bearer {}'.format(lmauth)}
+del lmauth
+
 payload = {'start_date': '2016-01-01', 'end_date': '2020-05-20'}
 
 r = requests.get('https://dev.lunchmoney.app/v1/transactions', params = payload, headers = headers)
@@ -24,14 +31,27 @@ amazon_purchases_nodupe = csv.DictReader(open('amazon_nodupe.csv', encoding='utf
 transaction_id = []
 transaction_note = []
 
+#Get any combined item transactions with same order ID
 for row in amazon_purchases_nodupe:
 	for x in amazon_transactions:
-		if float(x['amount']) == float(row['Total Price'].replace('$', '')):
+		if float(x['amount']) == float(row['Item Total'].replace('$', '')):
 			if timedelta(minutes=-2880) <= datetime.strptime(x['date'], '%Y-%m-%d') - datetime.strptime(row['Order Date'], '%m/%d/%y') <= timedelta(minutes=2880):
 				transaction_id.append(x['id'])
 				transaction_note.append(row['Title Cat'])
 
+amazon_purchases_single = csv.DictReader(open('amazon.csv', encoding='utf-8'))
+
+#Repeat without combining items to get items where Amazon did not combine items in the same order ID in one transaction
+for row in amazon_purchases_single:
+	for x in amazon_transactions:
+		if float(x['amount']) == float(row['Item Total'].replace('$', '')):
+			if timedelta(minutes=-2880) <= datetime.strptime(x['date'], '%Y-%m-%d') - datetime.strptime(row['Order Date'], '%m/%d/%y') <= timedelta(minutes=2880):
+				transaction_id.append(x['id'])
+				transaction_note.append(row['Title'])
+				
 for x in range(len(transaction_id)):
 	payload = {'transaction': {'notes': transaction_note[x]}}
 	r = requests.put("https://dev.lunchmoney.app/v1/transactions/{}".format(transaction_id[x]), json = payload, headers=headers)
 	print('Updating transaction ' + str(x+1) + '/' + str(len(transaction_id)))
+
+del headers
