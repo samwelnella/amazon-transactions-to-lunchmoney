dea4a5fbbca2158ed3c347d88c65dab3e3ef4974
samwelnella <danielsamwel@gmail.com>
2020-09-01 12:48:19 -0400

    Update update_lunchmoney_from_amazon.py

--- update_lunchmoney_from_amazon.py
+++ update_lunchmoney_from_amazon.py
@@ -4,51 +4,90 @@ import pandas
 import keyring
 from datetime import datetime
 from datetime import timedelta
+import calendar
 
 service_id = 'AMAZON_TO_LM'
+
+today = datetime.today()
+
+print('Time range for transaction import?')
+print('\n1) This month')
+print('2) Last month')
+print('3) Custom range\n')
+
+while True:
+	try:
+		selection = int(input('Please choose: '))
+	except ValueError:
+		print('Please pick a valid choice')
+		continue
+	if not 0 < selection < 4:
+		print('Please pick a valid choice')
+		continue
+	else:
+		break
+
+if selection == 1:
+	start_date = str(today.date().replace(day=1).isoformat())
+	end_date = str(today.date().isoformat())
+elif selection == 2:
+	start_date = today.date().replace(day=1)- timedelta(days=1)
+	start_date = str(start_date.replace(day=1).isoformat())
+	end_date = today.date().replace(day=1)- timedelta(days=1)
+	end_date = str(end_date.isoformat())
+elif selection == 3:
+	while True:
+		try:
+			start_date = str(input('\nPlease enter start date [YYYY-MM-DD]:'))
+			datetime.fromisoformat(start_date)
+		except ValueError:
+			print('Please format the date correctly')
+			continue
+		else:
+			break
+	while True:
+		try:
+			end_date = str(input('Please enter end date [YYYY-MM-DD]:'))
+			datetime.fromisoformat(end_date)
+		except ValueError:
+			print('Please format the date correctly')
+			continue
+		else:
+			break
+
+print(start_date)
+print(end_date)
 
 #Set your lmauth variable with keyring.set_password(service_id, 'lmauth', 'YOUR_LUNCH_MONEY_API_KEY_HERE')
 lmauth = keyring.get_password(service_id, 'lmauth')
 headers = {'Authorization': 'Bearer {}'.format(lmauth)}
 del lmauth
 
-payload = {'start_date': '2016-01-01', 'end_date': '2020-05-20'}
+payload = {'start_date': start_date, 'end_date': end_date}
 
 r = requests.get('https://dev.lunchmoney.app/v1/transactions', params = payload, headers = headers)
 transactions = r.json()
-amazon_transactions = [x for x in transactions['transactions'] if x['payee'] == 'Amazon']
-
-amazon_purchases = pandas.read_csv('amazon.csv', header = 0, encoding='utf8')
-amazon_purchases['Item Total'] = amazon_purchases['Item Total'].str.replace(',', '').str.replace('$', '').astype(float)
-amazon_purchases['Title'] = amazon_purchases['Title'].astype(str)
-amazon_purchases['Total Price'] = amazon_purchases.groupby(['Order ID'])['Item Total'].transform('sum')
-amazon_purchases['Title Cat'] = amazon_purchases.groupby(['Order ID'])['Title'].transform(lambda x: '; '.join(x))
-amazon_purchases = amazon_purchases.drop_duplicates(['Order ID'])
-amazon_purchases.to_csv('amazon_nodupe.csv', encoding='utf-8', index=False)
-
-amazon_purchases_nodupe = csv.DictReader(open('amazon_nodupe.csv', encoding='utf-8'))
+amazon_transactions = [x for x in transactions['transactions'] if x['payee'] == 'Amazon' or x['payee'] == 'Amazon Prime' or x['payee'] == 'Amazon Marketplace' or x['payee'] == 'AMAZON.COM']
+print(str(len(amazon_transactions)) + ' Lunch Money Amazon transactions found.')
 
 transaction_id = []
 transaction_note = []
-
-#Get any combined item transactions with same order ID
-for row in amazon_purchases_nodupe:
-	for x in amazon_transactions:
-		if float(x['amount']) == float(row['Item Total'].replace('$', '')):
-			if timedelta(minutes=-2880) <= datetime.strptime(x['date'], '%Y-%m-%d') - datetime.strptime(row['Order Date'], '%m/%d/%y') <= timedelta(minutes=2880):
-				transaction_id.append(x['id'])
-				transaction_note.append(row['Title Cat'])
 
 amazon_purchases_single = csv.DictReader(open('amazon.csv', encoding='utf-8'))
 
 #Repeat without combining items to get items where Amazon did not combine items in the same order ID in one transaction
 for row in amazon_purchases_single:
 	for x in amazon_transactions:
-		if float(x['amount']) == float(row['Item Total'].replace('$', '')):
-			if timedelta(minutes=-2880) <= datetime.strptime(x['date'], '%Y-%m-%d') - datetime.strptime(row['Order Date'], '%m/%d/%y') <= timedelta(minutes=2880):
-				transaction_id.append(x['id'])
-				transaction_note.append(row['Title'])
-				
+		payment_parse = row['payments'].split(':')
+		if len(payment_parse) == 3:
+			if float(x['amount']) == float(payment_parse[2].replace('$', '').replace(';', '').strip()):
+				if timedelta(minutes=-2880) <= datetime.strptime(x['date'], '%Y-%m-%d') - datetime.strptime(payment_parse[1].strip(), '%B %d, %Y') <= timedelta(minutes=2880):
+					transaction_id.append(x['id'])
+					transaction_note.append(row['items'])
+
+print(transaction_id)
+print(transaction_note)
+	
 for x in range(len(transaction_id)):
 	payload = {'transaction': {'notes': transaction_note[x]}}
 	r = requests.put("https://dev.lunchmoney.app/v1/transactions/{}".format(transaction_id[x]), json = payload, headers=headers)
